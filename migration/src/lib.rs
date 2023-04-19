0680dfa5ebded915e32f8d4f7fbda8168ea70930
greendrop <greendrop.devel@gmail.com>
2023-04-19 15:17:45 +0900

    commentsテーブルを追加

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,12 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20230415_030812_create_articles;
+mod m20230419_061011_create_comments;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20230415_030812_create_articles::Migration)]
+        vec![
+            Box::new(m20230415_030812_create_articles::Migration),
+            Box::new(m20230419_061011_create_comments::Migration),
+        ]
     }
 }
---
+++ migration/src/m20230419_061011_create_comments.rs
@@ -0,0 +1,41 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Comments::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Comments::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Comments::ArticleId).integer().not_null())
+                    .col(ColumnDef::new(Comments::Body).string().not_null())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Comments::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(Iden)]
+enum Comments {
+    Table,
+    Id,
+    ArticleId,
+    Body,
+}
